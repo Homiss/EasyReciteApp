7003cbca426fe29290d3f282b77cc2cb2da06fcf
Homiss <714788766@qq.com>
2017-07-12 22:12:56 +0800

    Add 完成背题方式修改交互

--- app/src/main/java/com/adm/dictionary/dictionary/HomeFragment.java
+++ app/src/main/java/com/adm/dictionary/dictionary/HomeFragment.java
@@ -47,7 +47,8 @@ public class HomeFragment extends BaseFragment {
     private JSONArray groupsArray;
     private String[] groups;
     private Integer currentGroupId;
-    private Integer currentPosition;
+    private Integer currentGroupPosition;
+    private Integer currentReciteModelPosition;
 
     private ViewPager viewpager;
     private Dialog dialog;
@@ -56,7 +57,7 @@ public class HomeFragment extends BaseFragment {
     private TextView groupNameTv;
     private TextView sumCountTv;
     private TextView modifyGroupTv;
-    private TextView modifyRecitewayTv;
+    private TextView modifyReciteModelTv;
 
     private Setting setting;
 
@@ -78,7 +79,7 @@ public class HomeFragment extends BaseFragment {
         groupNameTv = findTextViewbyId(v, R.id.frag_home_group_name);
         sumCountTv = findTextViewbyId(v, R.id.frag_home_sum_count);
         modifyGroupTv = findTextViewbyId(v, R.id.frag_home_modifygroup);
-        modifyRecitewayTv = findTextViewbyId(v, R.id.frag_home_reciteway);
+        modifyReciteModelTv = findTextViewbyId(v, R.id.frag_home_recitemodel);
     }
 
     public void initListener() {
@@ -106,8 +107,7 @@ public class HomeFragment extends BaseFragment {
                 }
             }
         });
-
-        modifyRecitewayTv.setOnClickListener(new View.OnClickListener() {
+        modifyReciteModelTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!HttpUtil.isNetworkAvailable(getActivity())) {
@@ -138,7 +138,7 @@ public class HomeFragment extends BaseFragment {
                     for(int i = 0; i < groupsArray.length(); i++){
                         groups[i] = groupsArray.optJSONObject(i).optString("name");
                         if(groupsArray.optJSONObject(i).optInt("id") == currentGroupId){
-                            currentPosition = i;
+                            currentGroupPosition = i;
                         }
                     }
                     initDialog();
@@ -176,6 +176,7 @@ public class HomeFragment extends BaseFragment {
                             setting.setReciteNum(obj.optJSONObject("data").optInt("reciteNum"));
                             setting.setSumCount(obj.optJSONObject("data").optInt("sumCount"));
                             setting.setHasReciteCount(obj.optJSONObject("data").optInt("hasReciteCount"));
+                            currentReciteModelPosition = setting.getReciteModel();
                             refresh();
                         } else {
                             if (obj.getString("returnCode").equals("403")) { // 跳转到登录界面
@@ -203,29 +204,58 @@ public class HomeFragment extends BaseFragment {
                 .setItems(groups, new CBDialogBuilder.onDialogItemClickListener() {
                     @Override
                     public void onDialogItemClick(CBDialogBuilder.DialogItemAdapter ItemAdapter, Context context, CBDialogBuilder dialogbuilder, Dialog dialog, int position) {
-                        currentPosition = position;
+                        currentGroupPosition = position;
                         groupNameTv.setText(groupsArray.optJSONObject(position).optString("name"));
                         currentGroupId = groupsArray.optJSONObject(position).optInt("id");
                         dialog.dismiss();
                         modifyReciteGroup();
                     }
-                }, currentPosition)
+                }, currentGroupPosition)
                 .showIcon(false).create();
         dialog.show();
     }
 
     private void recitewayDialog() {
         dialog = new CBDialogBuilder(getContext(), CBDialogBuilder.DIALOG_STYLE_NORMAL)
-                .setTitle("题库更换")
+                .setTitle("背题方式更换")
                 .setItems(new String[]{"顺序背题", "随机背题"}, new CBDialogBuilder.onDialogItemClickListener() {
                     @Override
                     public void onDialogItemClick(CBDialogBuilder.DialogItemAdapter ItemAdapter, Context context, CBDialogBuilder dialogbuilder, Dialog dialog, int position) {
-                        showToast(String.valueOf(position));
+                        currentReciteModelPosition = position;
                         dialog.dismiss();
+                        modifyReciteModel();
                     }
-                }, currentPosition)
+                }, currentReciteModelPosition)
                 .showIcon(false).create();
         dialog.show();
+    }
+
+    private void modifyReciteModel() {
+        HttpMethods.getInstance().modifyReciteModel(userId, token, currentReciteModelPosition).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<ResponseBody>() {
+            @Override
+            public void call(final ResponseBody res) {
+                JSONObject obj = null;
+                try {
+                    obj = new JSONObject(res.string());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (obj.optBoolean("success")) {
+                    initData();
+                    showToast(obj.optString("data"));
+                } else {
+                    if (obj.optString("returnCode").equals("403")) { // 跳转到登录界面
+                        Intent intent = new Intent(getContext(), LoginAndRegistActivity.class);
+                        startActivity(intent);
+                        return;
+                    }
+                    showToast("出错了～");
+                    return;
+                }
+            }
+        });
     }
 
     private void modifyReciteGroup() {
